94f3c3242d1ce17891f56f12d59b510947c167c0
bhrigubhuyan01 <bhrigubhuyan01@gmail.com>
2026-05-26 00:27:33 +0530

    fixed supabase auth

--- frontend/assets/js/app.js
+++ frontend/assets/js/app.js
@@ -1,6 +1,18 @@
 // Randhoni Application State Engine
 // Backed by LocalStorage for robust, production-ready demonstration
-import { supabase } from "../../config/supabase.js";
+import { createClient } from
+"https://esm.sh/@supabase/supabase-js";
+
+const supabaseUrl =
+"https://mvxkgarnskqmiljigddr.supabase.co";
+
+const supabaseKey =
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im12eGtnYXJuc2txbWlsamlnZGRyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk2MDMyODQsImV4cCI6MjA5NTE3OTI4NH0.7vf-a_rmc2QMqHv3IJ3pTxRb7bVay-MHHQ0btzAhyKs";
+
+const supabase = createClient(
+  supabaseUrl,
+  supabaseKey
+);
 import ASSAM_LOCATIONS from "../../data/assamlocations.js";
 const PLATFORM_DOMAIN = "randhoni.in";
 const API_BASE_URL = "https://randhoni.onrender.com/api/auth";
